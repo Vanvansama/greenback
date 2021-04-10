cfb7b88d729a7d057752b3f75506266e5e05e2fc
Stefan <953181215@qq.com>
2021-04-10 10:51:57 +0800

    update db

--- littlegreen.sql
+++ littlegreen.sql
@@ -1,43 +1,59 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : 本地数据库
- Source Server Type    : MariaDB
- Source Server Version : 100125
+ Source Server         : 本地mysql
+ Source Server Type    : MySQL
+ Source Server Version : 100509
  Source Host           : localhost:3306
  Source Schema         : littlegreen
 
- Target Server Type    : MariaDB
- Target Server Version : 100125
+ Target Server Type    : MySQL
+ Target Server Version : 100509
  File Encoding         : 65001
 
- Date: 05/04/2021 10:26:19
+ Date: 10/04/2021 10:51:25
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin`  (
+  `id` int NOT NULL,
+  `username` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `password` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `sex` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `phone` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `create_time` datetime NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of admin
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for bike
 -- ----------------------------
 DROP TABLE IF EXISTS `bike`;
---CREATE TABLE `bike`  (
---  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
---  `state` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
---  `location` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
---) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
-
-CREATE TABLE `bike` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `state` VARCHAR(45) NOT NULL,
-  `location` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`)
-);
+CREATE TABLE `bike`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `state` varchar(45) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `location` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `userId` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of bike
 -- ----------------------------
-INSERT INTO `bike` VALUES (1, 'close', '1234');
+INSERT INTO `bike` VALUES (1, 'close', '香洲区政府', NULL);
+INSERT INTO `bike` VALUES (2, 'close', ' ', NULL);
+INSERT INTO `bike` VALUES (3, 'close', ' ', NULL);
 
 -- ----------------------------
 -- Table structure for exception
@@ -48,32 +64,42 @@ CREATE TABLE `exception`  (
   `bikeId` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
   `errorInfo` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT '',
   `imgList` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT '',
-  `userId` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT ''
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+  `userId` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '',
+  `status` int NULL DEFAULT 0,
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of exception
 -- ----------------------------
-INSERT INTO `exception` VALUES ('', 'testId', '', '', 'testId');
+INSERT INTO `exception` VALUES ('香洲区政府(翠景路南)', '1', '123123123', '', 'oUd-C4jeaqBODgqY3nSDyPH4VRzY', 1, 1);
 
 -- ----------------------------
 -- Table structure for record
 -- ----------------------------
 DROP TABLE IF EXISTS `record`;
 CREATE TABLE `record`  (
-  `id` INT NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `begin_addr` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `end_addr` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `date` datetime(0) NULL DEFAULT NULL,
-  `bikeId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL,
+  `date` datetime NULL DEFAULT NULL,
+  `bikeId` int NOT NULL,
   `userId` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Compact;
+  `state` int NOT NULL DEFAULT 1 COMMENT '默认为1，表示没该路程还没结束，为0表示该路程已经结束\r\n',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of record
 -- ----------------------------
-INSERT INTO `record` VALUES (1, '北京理工大学珠海学院第三饭堂', '北京理工大学珠海学院34栋宿舍', '0000-00-00 00:00:00', 'testId', '');
+INSERT INTO `record` VALUES (5, '香洲区政府(翠景路南)', '华翠豪庭', '2021-04-10 08:30:34', 1, 'oUd-C4jeaqBODgqY3nSDyPH4VRzY', 0);
+INSERT INTO `record` VALUES (6, '香洲区政府(翠景路南)', '华翠豪庭', '2021-04-10 08:42:59', 1, 'oUd-C4jeaqBODgqY3nSDyPH4VRzY', 0);
+INSERT INTO `record` VALUES (10, '香洲区政府(翠景路南)', '香洲区政府(翠景路南)', '2021-04-10 09:02:33', 1, 'oUd-C4jeaqBODgqY3nSDyPH4VRzY', 0);
+INSERT INTO `record` VALUES (11, '香洲区政府(翠景路南)', '香洲区政府(翠景路南)', '2021-04-10 09:05:23', 1, 'oUd-C4jeaqBODgqY3nSDyPH4VRzY', 0);
+INSERT INTO `record` VALUES (12, '香洲区政府(翠景路南)', '香洲区政府(翠景路南)', '2021-04-10 09:06:30', 1, 'oUd-C4jeaqBODgqY3nSDyPH4VRzY', 0);
+INSERT INTO `record` VALUES (13, '香洲区政府(翠景路南)', '香洲区政府(翠景路南)', '2021-04-10 09:07:17', 1, 'oUd-C4jeaqBODgqY3nSDyPH4VRzY', 0);
+INSERT INTO `record` VALUES (14, '香洲区政府(翠景路南)', '香洲区政府', '2021-04-10 09:07:59', 1, 'oUd-C4jeaqBODgqY3nSDyPH4VRzY', 0);
 
 -- ----------------------------
 -- Table structure for report
@@ -85,13 +111,16 @@ CREATE TABLE `report`  (
   `errorInfo` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NULL DEFAULT NULL,
   `imgList` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `userId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+  `userId` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `status` int NULL DEFAULT 0,
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of report
 -- ----------------------------
-INSERT INTO `report` VALUES ('', 'testId', '123123', NULL, '', 'testId');
+INSERT INTO `report` VALUES ('香洲区政府(翠景路南)', '1', '123123123', 'http://tmp/CCBjZSaDK6vVeea29b5e42100b19161e56144612dcb4.jpg,http://tmp/cvjJiNJGVHPsf20a86bcbb39a8d1d20e54a977b85b75.png', '车把', 'oUd-C4jeaqBODgqY3nSDyPH4VRzY', 1, 1);
 
 -- ----------------------------
 -- Table structure for suggestion
@@ -100,7 +129,7 @@ DROP TABLE IF EXISTS `suggestion`;
 CREATE TABLE `suggestion`  (
   `detail` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `userId` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NULL DEFAULT NULL
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = Compact;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of suggestion
@@ -108,44 +137,26 @@ CREATE TABLE `suggestion`  (
 INSERT INTO `suggestion` VALUES ('123456', NULL);
 INSERT INTO `suggestion` VALUES ('123123', NULL);
 INSERT INTO `suggestion` VALUES ('2131', NULL);
+INSERT INTO `suggestion` VALUES ('12312313', 'oUd-C4jeaqBODgqY3nSDyPH4VRzY');
 
 -- ----------------------------
 -- Table structure for user
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
-  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
   `sex` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `phone` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL,
   `amout` double(255, 1) NULL DEFAULT NULL,
-  `id` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `id` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `college` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL
-) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = Compact;
-
--- ----------------------------
--- Table structure for admin
--- ----------------------------
-
-CREATE TABLE `admin` (
-  `id` int NOT NULL,
-  `username` varchar(45) DEFAULT NULL,
-  `password` varchar(45) DEFAULT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `sex` varchar(45) DEFAULT NULL,
-  `phone` varchar(45) DEFAULT NULL,
-  `create_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE = InnoDB CHARACTER SET = latin1 COLLATE = latin1_swedish_ci ROW_FORMAT = COMPACT;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('root', 'root', '', '', 0.0, '1', NULL);
-INSERT INTO `user` VALUES ('刘凡', '12345', 'male', '13192269125', 10.0, 'testId', '计算机');
-INSERT INTO `user` VALUES ('admin', 'admin', NULL, NULL, NULL, '', NULL);
+INSERT INTO `user` VALUES ('root', 'root', 'male', '', 0.0, '1', NULL);
+INSERT INTO `user` VALUES ('刘凡', '12345', 'male', '13192269125', 0.0, 'oUd-C4jeaqBODgqY3nSDyPH4VRzY', '计算机');
 
 SET FOREIGN_KEY_CHECKS = 1;
-
-
-
